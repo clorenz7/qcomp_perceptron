ad1e5b7cc263d626d24c7f311c4621836a59121b
Cory Lorenz <clorenz@gmail.com>
2020-06-05 14:31:36 -0700

    Docs, Commentary, Cleanup

--- final_project.py
+++ final_project.py
@@ -1,15 +1,21 @@
-import math
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from qiskit import QuantumCircuit, execute, Aer, IBMQ
-from qiskit.compiler import transpile, assemble
-# from qiskit.tools.jupyter import *
-# from qiskit.visualization import *
+from qiskit import QuantumCircuit, execute, Aer
 
 
 def code_to_sign_pattern(code):
+    """
+    Converts an integer code to its binary vector representation.
+    For instance, 7 = 0111 -> (-1)^[0,1,1,1] = [1, -1, -1, -1]
+
+    Currently only works for the 2 qubit case
+
+    inputs: integer,
+    outputs: list of +-1 values
+    """
+
     signs = [1]*4
     modulus = code
     for idx in range(4):
@@ -20,6 +26,22 @@ def code_to_sign_pattern(code):
     return signs
 
 def implement_HSGS_2qubit(circuit, signs, count_map):
+    """
+    Implementation of the hypergraph states generation subroutine for 2 qubits
+
+    HSGS is an efficient method for implementing the unitary transforms required
+    to both initialize a data vector, as well as the rotation which implements
+    the dot product of it with a weight vector
+
+    inputs:
+        circuit: work in progress IBM QuantumCircuit object
+        signs: list of signs on the computational basis states
+        count_map: (dict) maps the count of "1" qubits to
+            a list of the comp basis indexes with that number of "1" qubits
+
+    outputs:
+        circuit: IBM QuantumCircuit object with the appropriate unitary transform added to it
+    """
 
     # Define the binary representation of each state
     binary_reps = ["00", "01", "10", "11"]
@@ -48,7 +70,6 @@ def implement_HSGS_2qubit(circuit, signs, count_map):
                 if bin_rep[apply_qbit] == "1":
                     implemented_signs[i] *= -1
 
-
     # Apply the sign change for the states with 2 "1" qubits, eg |11>
     for idx in count_map.get(2, []):
         # See if the sign that is implemented is not correct
@@ -64,9 +85,23 @@ def implement_HSGS_2qubit(circuit, signs, count_map):
     return circuit
 
 def create_perceptron_circuit(data_code, weight_code):
+    """
+    Implements a quantum circuit to compute the perceptron output a data and weight vector
+
+    The data and weight vectors are binary, and specified by their integer code.
+    The circuit returned  is suitable for simulation or execution with IBM qiskit
+
+    inputs:
+        data_code (int)
+        weight_code (int)
+
+    outputs:
+        circuit qiskit QuantumCircuit Type
+    """
+
     # Initialize the quantum circuit: 3 qubits (2 state, 1 work), 1 cbit
     circuit = QuantumCircuit(3, 1)
-    # Set the 4 pixel qubits to equisuperposition state
+    # Set the 4 pixel qubits to equi-superposition state
     circuit.h(0)
     circuit.h(1)
 
@@ -81,7 +116,7 @@ def create_perceptron_circuit(data_code, weight_code):
 
     # Apply the unitary transform to initialize the data
     circuit = implement_HSGS_2qubit(circuit, data_signs, count_to_idxs)
-    # Apply the unitary transform to impliment dot product with weight vector
+    # Apply the unitary transform to implement dot product with weight vector
     circuit = implement_HSGS_2qubit(circuit, weight_signs, count_to_idxs)
 
     # Do the final H and X gates:
@@ -91,27 +126,18 @@ def create_perceptron_circuit(data_code, weight_code):
     circuit.x(1)
 
     # Final Toffoli gate
-    circuit.ccx(0,1,2)
+    circuit.ccx(0, 1, 2)
 
     # Measure the ancilla qubit
-    circuit.measure([2],[0])
+    circuit.measure([2], [0])
 
 
     return circuit
 
 
-# Define map of the #of ones in the binary rep to the indexes:
-# count_to_idxs = {
-#     1: [1, 2, 4, 8],
-#     2: [3, 5, 6, 9, 10, 12],
-#     3: [7, 11, 13, 14],
-#     4: [15],
-# }
-
-
 def main():
 
-    n_shots = 8192
+    n_shots = 8192  # Match the # of shots used in the paper
 
     # Use Aer's qasm_simulator
     simulator = Aer.get_backend('qasm_simulator')
@@ -121,9 +147,11 @@ def main():
     for data_code in range(16):
         for weight_code in range(16):
             circuit = create_perceptron_circuit(data_code, weight_code)
+
+            # Pause simulation to check circuit is implemented as in the paper
             if data_code == 11 and weight_code == 7:
-                import ipdb; ipdb.set_trace()
                 # circuit.draw()
+                import ipdb; ipdb.set_trace()
 
             # Execute the circuit on the qasm simulator
             job = execute(circuit, simulator, shots=n_shots)
@@ -133,17 +161,18 @@ def main():
 
             # Return counts
             counts = result.get_counts(circuit)
-            print("Total [0,1] counts for i={} and w={} are:".format(data_code, weight_code),counts)
+            print("Total counts for i={} and w={} are:".format(data_code, weight_code),counts)
 
+            # Store results in the matrix
             results_matrix[data_code, weight_code] = counts.get('1', 0)/n_shots
 
+    # Visualize the results as an image
     plt.imshow(results_matrix)
     plt.xlabel('Weight Code ($w_j$)')
     plt.ylabel('Data Code ($i_k$)')
     plt.colorbar()
     plt.title('Simulated Probabilities')
     plt.show()
-
 
 
 if __name__ == '__main__':
